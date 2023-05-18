e1069a68484f24d69156ca752fbf363a5cf44a17
oMatemátic <116760961+oMatematic@users.noreply.github.com>
2023-05-18 23:56:21 +0000

    Atualização do servidor

--- Server.py
+++ Server.py
@@ -5,6 +5,7 @@
 # importando a biblioteca
 import socket
 import time
+import csv
 print("Eu sou o SERVIDOR UDP!")
 
 # definindo ip e porta
@@ -17,21 +18,42 @@ servidor = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 servidor.bind((HOST, PORT))
 
 
-print("Aguardando cliente...")
+ 
+def lerTxt():
+    file = open("resd.txt","r") 
+    Counter = 0
+    Content = file.read() 
+    CoList = Content.split("\n") 
+    
+    for i in CoList: 
+        if i: 
+            Counter += 1
 
-while (True):
-	print("-----")
-	# cliente conectou - recuperando informações do cliente
-	msg, enderecoCliente = servidor.recvfrom(9000)
-	print(f"Cliente {enderecoCliente} enviou mensagem")
-	
-	mensagem = msg.decode("utf-8")
-	print(f"Mensagem enviada pelo cliente: {mensagem}")
-	print(f"Este servidor vai devolver a mensagem ao cliente {enderecoCliente}")
-	mensagem="mensagem enviada padrão"
-	
-	servidor.sendto(mensagem.encode("utf-8"), enderecoCliente)
+    return Counter
 
+
+
+def lerTxt():
+    with open('resd.txt', 'r') as f:
+        return f.read()
+a=2
+while a > 1:
+    print("-----")
+    # cliente conectou - recuperando informações do cliente
+    msg, enderecoCliente = servidor.recvfrom(9000)
+    print(f"Cliente {enderecoCliente} enviou mensagem")
+
+    mensagem = msg.decode("utf-8")
+    teste = mensagem[0]
+    print(teste)
+    if teste == '2':
+        with open('resd.txt', 'a') as f:
+            f.write(mensagem + "\n")
+        resposta = "salvo com sucesso"
+        servidor.sendto(resposta.encode("utf-8"), enderecoCliente)
+
+    else:
+        servidor.sendto(lerTxt().encode("utf-8"), enderecoCliente)
 
 print("Encerrando o servidor...")
 servidor.close()
